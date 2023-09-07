ba0dc24c44f445381f6b397659fcd2f8477e37f6
Asep <asepsuherman389@gmail.com>
2023-09-07 11:01:06 +0700

    Changed format file js to jsx (App)

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+// import Navbar from './Component/Organism/Nav/Navbar'
+// import Footer from './Component/Organism/Nav/Footer'
+import Standard from './Component/Layout/Standard';
+import Hero from './Component/Organism/Content/Hero';
+import Event from './Component/Organism/Content/Event';
+import WorkProgram from './Component/Organism/Content/WorkProgram';
+import Experience from './Component/Organism/Content/Experience';
+import Contact from './Component/Organism/Content/Contact';
+
+function App({user}) {
+  return (
+    <>
+    <Standard>
+      <Hero/>
+      <Event/>
+      <WorkProgram/>
+      <Experience/>
+      <Contact/>
+    </Standard>
+    </>
+  );
+}
+
+export default App;

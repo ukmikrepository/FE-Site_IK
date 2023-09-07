b6a2d7a67c202a70f93a8c050b32aaf269b21cd1
Asep <asepsuherman389@gmail.com>
2023-09-07 11:51:38 +0700

    Add conteiner css

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,15 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    container: {
+      padding: {
+        DEFAULT: "1.25rem",
+        sm: "0.75rem",
+        lg: "3rem",
+        xl: "3rem",
+        "2xl": "3rem",
+      },
+    },
   },
   plugins: [],
 }

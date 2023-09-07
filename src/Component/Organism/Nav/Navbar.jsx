2c9066549987be748f5ada741919e42afd8b9ba0
yandiaan <diansetiawan2121@gmail.com>
2023-09-07 20:30:06 +0700

    navbar progress

--- src/Component/Organism/Nav/Navbar.jsx
+++ src/Component/Organism/Nav/Navbar.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='container flex bg-transparent w-full absolute'>
+      <img src="img/logo_ukmik.png" alt="" className='border border-primary-1 w-[75px] h-[75px]' />
+      <div className="flex">
+        <ul className='flex'>
+          <li>Home</li>
+          <li>Profile</li>
+          <li>Blog</li>
+        </ul>
+       </div>
+    </div>
   )
 }

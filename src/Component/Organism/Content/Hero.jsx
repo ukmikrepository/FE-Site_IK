450c900b4bdeb9525737468ea4f9f305a7f35b2a
Asep <asepsuherman389@gmail.com>
2023-09-07 11:09:08 +0700

    Recommit Section (Landing)

---
+++ src/Component/Organism/Content/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <>
+      <div>
+        <img src="img/Hero_BG.png" alt="" />
+      </div> 
+    </>
+  )
+}
---
+++ src/Component/Organism/Content/WorkProgram.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function WorkProgram() {
+  return (
+    <div>WorkProgram</div>
+  )
+}

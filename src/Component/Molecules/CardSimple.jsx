ff9713d98e296bd8fe3282c33cb3e323e559c191
Asep <asepsuherman389@gmail.com>
2023-09-08 21:04:13 +0700

    Add CardSimple

--- src/Component/Molecules/CardSimple.jsx
+++ src/Component/Molecules/CardSimple.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const CardSimple = ({ className = "", children }) => {
     return (
         <div
-            className={`flex flex-col px-2 py-3 bg-grey-10 md:px-4 md:py-5 rounded-3xl md:rounded-[36px] shadow-[0px_1px_8px_rgba(0,0,0,0.08)] w-96${className}`}
+            className={`flex flex-col px-2 py-3 bg-white md:px-4 md:py-5 rounded-3xl md:rounded-md shadow-[0px_6px_1px_rgba(205,205,205,1)] w-max${className}`}
         >
             {children}
         </div>

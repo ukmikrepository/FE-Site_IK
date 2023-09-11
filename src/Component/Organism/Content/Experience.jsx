ea22b7341370bad31e98a410cf49c326b8c7766f
yandiaan <diansetiawan2121@gmail.com>
2023-09-11 08:00:24 +0700

    change font experience

--- src/Component/Organism/Content/Experience.jsx
+++ src/Component/Organism/Content/Experience.jsx
@@ -33,7 +33,7 @@ export default function Experience() {
               <div className="flex items-center">
                 <CardSimple className="w-[250px] bg-primary-5 gap-4 drop-shadow-[-4px_-4px_4px_rgba(85,183,255,1)]">
                   <img src="/img/studyclub.png" className="w-[100px]" alt="" />
-                  <h1 className="text-white text-md w-3/4">Bergabunglah dengan UKM IK untuk meraih prestasi, ikuti lomba dan event, serta berbagi pengetahuan dengan banyak anggota berpengalaman.</h1>
+                  <h1 className="text-white md:text-sm text-md w-3/4">Bergabunglah dengan UKM IK untuk meraih prestasi, ikuti lomba dan event, serta berbagi pengetahuan dengan banyak anggota berpengalaman.</h1>
                   <span className="font-thin text-grey-9 italic">Event & Lomba</span>
                 </CardSimple>
               </div>
@@ -41,12 +41,12 @@ export default function Experience() {
               <div className="flex flex-col items-center justify-between gap-4">
                 <CardSimple className="w-[250px] bg-[#8E1AE0] gap-4 drop-shadow-[4px_4px_4px_#5E147C]">
                   <img src="/img/ikdev.png" className="w-[100px]" alt="" />
-                  <h1 className="text-white text-md w-3/4">Terjun langsung ke tim IK-Dev untuk belajar melalui pengalaman nyata dalam studi kasus yang inspiratif.</h1>
+                  <h1 className="text-white md:text-sm text-md w-3/4">Terjun langsung ke tim IK-Dev untuk belajar melalui pengalaman nyata dalam studi kasus yang inspiratif.</h1>
                   <span className="font-thin text-grey-9 italic">IK Dev</span>
                 </CardSimple>
                 <CardSimple className="w-[250px] bg-primary-3 gap-4 drop-shadow-[4px_4px_4px_#14008E]">
                   <img src="/img/santai.png" className="w-[100px]" alt="" />
-                  <h1 className="text-white text-md w-3/4">Di UKM IK, kami tidak hanya belajar, tetapi juga merayakan kesenangan bersama, seperti camping dan berolahraga bersama.</h1>
+                  <h1 className="text-white md:text-sm text-md w-3/4">Di UKM IK, kami tidak hanya belajar, tetapi juga merayakan kesenangan bersama, seperti camping dan berolahraga bersama.</h1>
                   <span className="font-thin text-grey-9 italic">Refreshing</span>
                 </CardSimple>
               </div>
